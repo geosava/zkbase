56346ed85b38ac3863be3c24233c5ac4b3c5f5e2
philipp.potisk <philipp.potisk@68bc690a-ca9a-11dd-bc61-0ff7aae23933>
2009-02-05 08:50:05 +0000

    generic list + bandbox-test added (needs tidying up)

--- trunk/src/main/java/org/zkbase/service/RoleService.java
+++ trunk/src/main/java/org/zkbase/service/RoleService.java
@@ -1,10 +1,19 @@
 package org.zkbase.service;
 
+import java.util.List;
+
 import org.zkbase.model.Role;
 
 public class RoleService extends GenericService<Role> {
 
 	public RoleService() {
 		super(Role.class);
+	}
+
+	@Override
+	public List<Role> findByExample(Role example, int firstResult,
+			int maxResults) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
