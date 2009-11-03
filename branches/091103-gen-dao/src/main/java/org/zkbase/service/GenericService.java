367976a4bf5dee264716977aa86e3a9bc02f4934
philipp.potisk <philipp.potisk@68bc690a-ca9a-11dd-bc61-0ff7aae23933>
2009-11-03 22:39:25 +0000

    RoleDaoTest and RoleServiceTest are working

--- branches/091103-gen-dao/src/main/java/org/zkbase/service/GenericService.java
+++ branches/091103-gen-dao/src/main/java/org/zkbase/service/GenericService.java
@@ -3,97 +3,84 @@ package org.zkbase.service;
 import java.io.Serializable;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.management.relation.Role;
+
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import org.zkbase.dao.BasicDao;
-import org.zkbase.dao.EntityNotFoundException;
-import org.zkbase.model.User;
+import org.zkbase.dao.BaseDao;
+
+import com.trg.search.ISearch;
+import com.trg.search.Search;
 
 @Transactional(readOnly=true,propagation = Propagation.REQUIRED)
 public abstract class GenericService<T>  {
 	
-	private final Class<T> objectClass;
+	private BaseDao baseDao;
 	
-	public GenericService(final Class<T> objectClass){
-		this.objectClass = objectClass;
-	}
+	public GenericService(BaseDao baseDao){
+		this.baseDao = baseDao;
+		//this.objectClass = objectClass;
+	}	
 	
-	@Autowired
-	private BasicDao basicDao;
-	
-
-	/* (non-Javadoc)
-	 * @see org.zkbase.service.BaseService#count()
-	 */
-	public Long count() {
-		return this.basicDao.count(this.objectClass);
+	public GenericService() {
+		// needed for aop cglib 
 	}
 
-	/* (non-Javadoc)
-	 * @see org.zkbase.service.BaseService#findByID(java.io.Serializable)
-	 */
+	public int count() {
+		return this.baseDao.count(new Search());
+	}
+
 	@SuppressWarnings("unchecked")	
-	public T findByID(Serializable id) throws EntityNotFoundException {
-		return basicDao.find(objectClass, id);
+	public T findById(Serializable id) {
+		return (T) baseDao.find(id);
 	}
 
-	/* (non-Javadoc)
-	 * @see org.zkbase.service.BaseService#findAll()
-	 */
 	@SuppressWarnings("unchecked")	
 	public List<T> findAll(){
-		List<?> objects = this.basicDao.findAll(objectClass);
-		return(List<T>) objects;
+		return(List<T>) this.baseDao.findAll();
 	}
 	
 	/* (non-Javadoc)
 	 * @see org.zkbase.service.BaseService#findAll(int, int)
 	 */
-	@SuppressWarnings("unchecked")
-	public List<T> findAll(int firstResult, int maxResults){
-		List<?> objects = this.basicDao.findAll(objectClass, firstResult, maxResults);
-		return(List<T>) objects;
-	}	
-	
-	@SuppressWarnings("unchecked")
-	protected <T> List<T> findByNamedQuery(String namedQuery, int firstResult, int maxResults, Object... params) {
-		return this.basicDao.findNamedQuery(namedQuery, firstResult, maxResults, params);		
-	}
-	
-	@SuppressWarnings("unchecked")
-	protected Object findByNamedQuerySingle(String namedQuery, Object... params){
-		return this.basicDao.findNamedQuerySingle(namedQuery, params);		
-	}
-	
-	/* (non-Javadoc)
-	 * @see org.zkbase.service.BaseService#persist(T)
-	 */
-	@Transactional(readOnly=false,propagation = Propagation.REQUIRED)
-	public void persist(T object){
-		this.basicDao.persist(object);
+//	@SuppressWarnings("unchecked")
+//	public List<T> findAll(int firstResult, int maxResults) {
+//		List<?> objects = this.baseDao.findAll(objectClass, firstResult,
+//				maxResults);
+//		return (List<T>) objects;
+//	}
+//
+//	@SuppressWarnings("unchecked")
+//	protected <T> List<T> findByNamedQuery(String namedQuery, int firstResult,
+//			int maxResults, Object... params) {
+//		return this.baseDao.findNamedQuery(namedQuery, firstResult, maxResults,
+//				params);
+//	}
+//
+//	@SuppressWarnings("unchecked")
+//	protected Object findByNamedQuerySingle(String namedQuery, Object... params) {
+//		return this.baseDao.findNamedQuerySingle(namedQuery, params);
+//	}
+//
+
+	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
+	public void persist(T object) {
+		this.baseDao.persist(object);
 	}
 
-	@Transactional(readOnly=false,propagation = Propagation.REQUIRED)
-	public void delete(Serializable id) throws EntityNotFoundException {
-		this.basicDao.remove(objectClass, id);
+	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
+	public boolean delete(Serializable id)  {
+		return this.baseDao.removeById(id);
 	}
 
-	/* (non-Javadoc)
-	 * @see org.zkbase.service.BaseService#merge(T)
-	 */
-	@Transactional(readOnly=false,propagation = Propagation.REQUIRED)
-	public void merge(T object) throws EntityNotFoundException {
-		this.basicDao.merge(object);
+	@Transactional(readOnly = false, propagation = Propagation.REQUIRED)
+	public void merge(T object) {
+		this.baseDao.merge(object);
 	}
-	
-	public void test(User u) {
-		Long x = (Long)this.basicDao.findSingle("SELECT COUNT(u) from User u WHERE u.username LIKE ?1", u.getUsername());
-		System.out.println(x);
-	}
-	
-	abstract public List<T> findByExample(T example, int firstResult, int maxResults);
-	
-	abstract public Long countByExample(T example);	
+
+//	abstract public List<T> findByExample(T example, int firstResult,
+//			int maxResults);
+//
+//	abstract public Long countByExample(T example);	
 
 }
